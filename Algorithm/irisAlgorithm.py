ed8cecf03914f18945b9f421e8d1b30a547c6d1e
FarzadBT <farzad.besharati.t@gmail.com>
2022-03-16 15:58:11 +0100

    Encrypted simple iris training
    Co-authored-by: Stefan Chan <Flakt@users.noreply.github.com>

--- Algorithm/irisAlgorithm.py
+++ Algorithm/irisAlgorithm.py
@@ -1,12 +1,13 @@
 import tenseal as ts
 import copy
+import pandas as pd
 from tqdm import tqdm
 import torch
 from torch import nn
-from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split, TensorDataset
 import torch.nn.functional as F
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 from simpleModel import SimpleModel
 
@@ -30,33 +31,47 @@ context.generate_galois_keys()
 
 
 ##### Hyperparameters for federated learning #########
-num_clients = 20
-num_selected = 6
-num_rounds = 150
+num_clients = 3
+num_selected = 2
+num_rounds = 200
 epochs = 10
 batch_size = 1024
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-train_data = datasets.MNIST("./Algorithm/", train=True, transform=transforms.ToTensor())
+# Preprocessing
+def encode_species(species):
+    if species == 'setosa':
+        return 0
+    if species == 'versicolor':
+        return 1
+    if species == 'virginica':
+        return 2
+    else:
+        raise ValueError('Species \'{}\' is not recognized.'.format(species))
 
-train_data.data = train_data.data.float() / 255
-N, H, W = train_data.data.shape
-train_data.data = train_data.data.reshape((N, 1, H, W))
+dataset = pd.read_csv("Algorithm/iris.csv")
 
-#train_set = TensorDataset(train_data.data.to(device), train_data.targets.to(device))
-train_set = TensorDataset(train_data.data, train_data.targets)
-train_data_split = random_split(train_set, [int(len(train_set) / num_clients) for _ in range(num_clients)])
-train_loader = [DataLoader(x, batch_size=batch_size, shuffle=True) for x in train_data_split]
+x = dataset[['sepal_length', 'sepal_width', 'petal_length', 'petal_width']].values
+y = np.array(list(map(encode_species, dataset['species'].values)))
 
-test_data = datasets.MNIST("./Algorithm/", train=False, transform=transforms.ToTensor())
-test_loader = DataLoader(test_data, batch_size=2048, shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=10)
+
+torch_x = torch.tensor(x_train, dtype=torch.float32)
+torch_y = torch.tensor(y_train, dtype=torch.int64)
+t_dataset = TensorDataset(torch_x, torch_y)
+t_data_split = random_split(t_dataset, [int(len(t_dataset) / num_clients) for _ in range(num_clients)])
+t_data_loaders = [DataLoader(x, batch_size=32, shuffle=True) for x in t_data_split]
+
+torch_x = torch.tensor(x_test, dtype=torch.float32)
+torch_y = torch.tensor(y_test, dtype=torch.int64)
+test_dataset = TensorDataset(torch_x, torch_y)
+test_loader = DataLoader(test_dataset, batch_size=32)
 
 
-
+# Helper functions
 def update_vendor(client_model, optimizer, train_loader, loss_fn, epoch=5):
     client_model.train()
-    for e in tqdm(range(epochs)):
+    for e in range(epochs):
         for data, target in train_loader:
             optimizer.zero_grad()
             output = client_model(data)
@@ -64,31 +79,41 @@ def update_vendor(client_model, optimizer, train_loader, loss_fn, epoch=5):
             loss.backward()
             optimizer.step()
 
-    client_dict = client_model.state_dict()
+    client_dict = copy.deepcopy(client_model.state_dict())
     for key in client_dict:
-        print(client_dict[key][0])
         client_dict[key] = ts.ckks_tensor(context, client_dict[key])
 
     return (loss.item(), client_dict)
+
+def average_weights(global_model, vendor_models):
+    w_avg = copy.deepcopy(vendor_models[0].state_dict())
+
+    for key in w_avg.keys():
+        for i in range(1, len(vendor_models)):
+            w_avg[key] += vendor_models[i].state_dict()[key]
+        w_avg[key] = torch.div(w_avg[key], len(vendor_models))
+
+    global_model.load_state_dict(w_avg)
+    for model in vendor_models:
+        model.load_state_dict(global_model.state_dict())
 
 def average_enc_weights(vendor_weights):
     w_avg = copy.deepcopy(vendor_weights[0])
 
     for key in w_avg.keys():
         for i in range(1, len(vendor_weights)):
-            w_avg[key] += vendor_weights[i].state_dict()[key]
-        w_avg[key] = torch.div(w_avg[key], len(vendor_weights))
+            w_avg[key] += vendor_weights[i][key]
+        w_avg[key] = w_avg[key] * (1/len(vendor_weights))
 
     return w_avg
 
 def test(global_model, test_loader):
-    """This function test the global model on test data and returns test loss and test accuracy """
+    #This function test the global model on test data and returns test loss and test accuracy
     model.eval()
     test_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
-            data, target = data.cuda(), target.cuda()
             output = global_model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
@@ -101,17 +126,15 @@ def test(global_model, test_loader):
 
 
 
-#global_model = SimpleModel().to(device)
 global_model = SimpleModel()
 
-#vendor_models = [SimpleModel().to(device) for _ in range(num_clients)]
 vendor_models = [SimpleModel() for _ in range(num_clients)]
 for model in vendor_models:
     model.load_state_dict(global_model.state_dict())
 
 vendor_optimizers = [torch.optim.Adam(model.parameters(), lr=0.01) for _ in range(num_clients)]
 
-loss_fns = [nn.NLLLoss() for _ in range(num_clients)]
+loss_fns = [nn.CrossEntropyLoss() for _ in range(num_clients)]
 
 for r in range(num_rounds):
     print(f"Round {r+1}")
@@ -121,16 +144,16 @@ for r in range(num_rounds):
     loss = 0
     enc_vendor_weights = []
     for i in range(num_selected):
-        print(f"Vendor {i+1} update")
+        #print(f"Vendor {i+1} update")
         client = selected_clients[i]
-        (tempLoss, tempEnc) = update_vendor(vendor_models[client], vendor_optimizers[client], train_loader[client], loss_fns[client], epochs)
+        (tempLoss, tempEnc) = update_vendor(vendor_models[client], vendor_optimizers[client], t_data_loaders[client], loss_fns[client], epochs)
         loss += tempLoss
         enc_vendor_weights.append(tempEnc)
 
     w_avg = average_enc_weights(enc_vendor_weights)
+
     for key in w_avg:
-        w_avg[key] = w_avg[key].decrypt()
-    
+        w_avg[key] = torch.Tensor(w_avg[key].decrypt().tolist())
     for model in vendor_models:
         model.load_state_dict(w_avg)
 
--- Algorithm/simpleModel.py
+++ Algorithm/simpleModel.py
@@ -1,20 +1,10 @@
 from torch import nn
-import torch.nn.functional as F
 
+# Our model inherits from `nn.Module`
 class SimpleModel(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=20, kernel_size=5)
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5)
-        self.fc1 = nn.Linear(4*4*50, 50)
-        self.fc2 = nn.Linear(50, 10)
-        
+        self.lin = nn.Linear(4, 3)
+
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, kernel_size=2)
-        x = x.view(-1, 4*4*50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+        return self.lin(x)
